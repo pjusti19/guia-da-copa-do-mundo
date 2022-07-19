f5bf5f899a50acf1bf7d74a656ccc4455b85b38a
paulistaLH <pedrinhojusti@gmail.com>
2022-07-18 22:24:30 -0300

    pagina das fontes e animação

--- animação.js
+++
@@ -1,5 +0,0 @@
-let tempo = 3000; 
-            setTimeout(() => {
-              document.querySelector('.desapareça').classList.add('sumir');
-              document.querySelector('#corpo').classList.remove('sumir');
-            }, tempo);
